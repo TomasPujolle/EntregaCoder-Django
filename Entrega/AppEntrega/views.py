b7de8f464f543ef71b8270cf5e7ddcc2bfefc3ce
Tomas Pujolle <alan_pujolle@hotmail.com>
2023-06-09 15:21:34 -0300

    vistas desde proyecto y app separadas

--- Entrega/AppEntrega/views.py
+++ Entrega/AppEntrega/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Template, Context
 
 
 def inicio(request):   
-    return HttpResponse(request, "AppEntrega/index.html")
+    return render(request, "AppEntrega/index.html")
 
 
